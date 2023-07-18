70f339fe342586095738fd832d604900cf957356
Mang1s <ivan1999143@gmail.com>
2023-07-18 20:09:55 +0300

    second

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,17 +1,22 @@
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import { useStore } from 'vuex';
+import { displayModes } from '../constants/display-modes';
 const props = defineProps(['TITLE', 'IMDB_ID', 'RANK', 'ACTORS', 'AKA', 'IMG_POSTER'])
+const store = useStore()
 </script>
 <template>
    <router-link :to="'/movies/' + IMDB_ID" class="movie-card">
       <div>
          <img :src=IMG_POSTER :alt=TITLE width="300" />
-         <h2>{{TITLE}}</h2>
-         <p>AKA: {AKA}</p>
+         <h2>{{ TITLE }}</h2>
+         <p>AKA: {{ AKA }}</p>
+         <p v-if="store.state.displayMode === displayModes.list">
+            <p>IMDB Ranked #{{ RANK }}</p>
+            <p>Featuring: {{ ACTORS }}</p>
+         </p>
       </div>
    </router-link>
 </template>
-<style scoped lang="scss">
-   
-</style>
+<style scoped lang="scss"></style>
